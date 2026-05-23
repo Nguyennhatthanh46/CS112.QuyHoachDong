f7829b4fd2aa5dbf9f5b6348022302c2e8b05084
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2026-05-23 23:54:26 +0700

    Duong di an toan

---
+++ Bai 07. Duong di an toan.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<string> grid(n);
+    for(int i = 0; i < n; i++) cin >> grid[i];
+
+    vector<vector<long long>> dp(n, vector<long long>(n, 0));
+
+    if(grid[0][0] == '.')
+        dp[0][0] = 1;
+
+    for(int i = 1; i < n; i++){
+        if(grid[i][0] == '.')
+            dp[i][0] = dp[i-1][0];
+        else
+            dp[i][0] = 0;
+    }
+
+    for(int j = 1; j < n; j++){
+        if(grid[0][j] == '.')
+            dp[0][j] = dp[0][j-1];
+        else
+            dp[0][j] = 0;
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
+            if(grid[i][j] == '*')
+                dp[i][j] = 0;
+            else
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+        }
+    }
+
+    cout << dp[n-1][n-1];
+    return 0;
+}
